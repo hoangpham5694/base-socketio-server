9fbc99c7b43be37532244682086adb3654549e84
Hoang <hoang.pm@neo-lab.vn>
2020-05-04 16:57:44 +0700

    connect db send message

--- src/handlers/messageHandler.js
+++ src/handlers/messageHandler.js
@@ -5,6 +5,11 @@ const format = require('string-format')
 const routes = require('../constants/apis')
 const events = require('../constants/events')
 const serverComponent = require('../components/serverComponent')
+const PgClient = require('../helpers/pgClient')
+const SystemHandler = require('../handlers/systemHandler')
+const notificationError = require('../constants/notificationError')
+const notificationSuccess = require('../constants/notificationSuccess')
+
 
 module.exports = class MessageHandler {
     constructor(socket, io) {
@@ -12,6 +17,7 @@ module.exports = class MessageHandler {
         this.io = io
         this.socket.component = {}
         this.serverComponent = new serverComponent()
+        this.systemHandler = new SystemHandler(this.socket, this.io)
     }
 
     requestSendMessage(data, room) {
@@ -19,11 +25,12 @@ module.exports = class MessageHandler {
         this.socket.component.server = this.serverComponent
         this.axiosClient = new AxiosHelper()
         this.sendMessagePromise(data, room).then(function(){
-            console.log("send message");
+            console.log("send message success");
+            this.systemHandler.responseSuccessNotification(notificationSuccess.SEND_MESSAGE_SUCCESS);
 
         }).catch(function(error){
            console.log("can not send message");
-           console.log("error");
+           this.systemHandler.responseErrorNotification(notificationError.SEND_MESSAGE_ERROR);
         })
 
     }
@@ -31,13 +38,24 @@ module.exports = class MessageHandler {
         return new Promise((resolve, reject) =>{
             if(this.socket.rooms[room] ){
                 this.getRoomData(room).then(function(roomData){
-                    this.callApiSendMessage(roomData,data).then(function(response){
-                        this.io.sockets.to(room).emit("receiver_message", {
-                            msg: response});
-                        this.checkMemberForPushNoti(room);
 
-                    }.bind(this)).catch(function(error){
-                        reject(error)
+                    var pgClient = new PgClient();
+                    var param = {
+                        room_id: roomData.id,
+                        content: data,
+                        user_id : this.socket.client.user.user_id,
+                        user_type : this.socket.client.user.user_type,
+                        socket_id : this.socket.id,
+                    };
+                    pgClient.createMessage(param, {
+                        done: (result) => {
+                            this.io.sockets.to(room).emit("receiver_message", {msg: result});
+                            this.checkMemberForPushNoti(room);
+
+                        },
+                        fail: (error) => {
+                            reject(error);
+                        }
                     });
 
                 }.bind(this)).catch(function(error){
@@ -69,30 +87,30 @@ module.exports = class MessageHandler {
             });
         })
     }
-    callApiSendMessage(roomData, msg){
-         return new Promise((resolve, reject)=>{
-             this.axiosClient.request({
-                 url: routes.API_SEND_MESSAGE,
-                 method: 'POST',
-                 data: {
-                     user_id : this.socket.client.user.user_id,
-                     user_type : this.socket.client.user.user_type,
-                     room_id: roomData.id,
-                     socket_id : this.socket.id,
-                     content: msg
-                 }
-             }, {
-                 done: (response) => {
-                     resolve(response.data);
-                 },
-                 fail: (error) => {
-                     console.log('Error')
-                     reject(error);
-
-                 }
-             })
-         })
-    }
+    // callApiSendMessage(roomData, msg){
+    //      return new Promise((resolve, reject)=>{
+    //          this.axiosClient.request({
+    //              url: routes.API_SEND_MESSAGE,
+    //              method: 'POST',
+    //              data: {
+    //                  user_id : this.socket.client.user.user_id,
+    //                  user_type : this.socket.client.user.user_type,
+    //                  room_id: roomData.id,
+    //                  socket_id : this.socket.id,
+    //                  content: msg
+    //              }
+    //          }, {
+    //              done: (response) => {
+    //                  resolve(response.data);
+    //              },
+    //              fail: (error) => {
+    //                  console.log('Error')
+    //                  reject(error);
+    //
+    //              }
+    //          })
+    //      })
+    // }
 
     checkMemberForPushNoti(room){
         return new Promise((resolve, reject) => {
