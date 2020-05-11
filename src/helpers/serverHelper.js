1254090b00ff42a4ec4453096d9ef110b59e40f8
Hoang <hoang.pm@neo-lab.vn>
2020-05-11 14:56:14 +0700

    refactor code

--- src/helpers/serverHelper.js
+++ src/helpers/serverHelper.js
@@ -1,18 +1,41 @@
+const UserManagermentComponent =  require('../components/userManagerComponent')
 
 module.exports = class ServerHelper{
 
     static disconnectOldUser(socket, io){
-
+        var userManagerment = new UserManagermentComponent()
         console.log("current socket: " + socket.id);
-        for(var id in io.of("/").connected){
-            var s = io.of("/").connected[id];
-            if(s.client.user === undefined){
-                s.disconnect();
-                continue;
-            }
-            if(s.client.user.access_token === socket.client.user.access_token && s.id !== socket.id){
-                s.disconnect();
-            }
-        }
+     //   console.log(io.of("/"ts());
+        io.of("/").adapter.clients((err, clients)=>{
+            console.log(clients);
+            clients.forEach(function(clientId){
+                userManagerment.isExistUser(clientId, {
+                    done: (result) => {
+                        console.log(result)
+                        userManagerment.getUser(clientId,{
+                            done:(user) =>{
+                                user = JSON.parse(user);
+                                console.log(user);
+                               if(user.access_token === socket.client.user.access_token && clientId !== socket.id){
+                                   io.of("/").adapter.remoteDisconnect(clientId, true);
+                               }
+                            }
+                        })
+                    }
+                })
+
+            })
+        })
+     //    for(var id in io.of("/").connected){
+     //      // console.log("dxx"+id);
+     //        var s = io.of("/").connected[id];
+     //        if(s.client.user === undefined){
+     //            s.disconnect();
+     //            continue;
+     //        }
+     //        if(s.client.user.access_token === socket.client.user.access_token && s.id !== socket.id){
+     //            s.disconnect();
+     //        }
+     //    }
     }
 }
